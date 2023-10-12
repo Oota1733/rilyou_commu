fe5f6dd8230db979091432383b63ffec0eadd8c5
Fuka <140349248+Oota1733@users.noreply.github.com>
2023-10-12 10:44:28 +0000

    修正3

--- db/migrate/20231008065622_create_entry_users.rb
+++ db/migrate/20231008065622_create_entry_users.rb
@@ -1,9 +1,10 @@
 class CreateEntryUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :entry_users do |t|
-      
-      t.references :user, foreign_key: true
-      t.references :commumity, foreign_key: true
+
+      t.references :user, type: :integer, foreign_key: true
+
+      t.references :commumity, type: :integer, foreign_key: true
 
       t.timestamps
     end
