91ba4d1853a9fa9da2df3fa6af35c0eea79910d1
Fuka <140349248+Oota1733@users.noreply.github.com>
2023-10-12 10:58:15 +0000

    修正8

--- db/migrate/20231008065707_create_posts.rb
+++ db/migrate/20231008065707_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :title,              null: false, default: ""
-      t.text :body,              null: false, default: ""
+      t.text :body,              null: false
       t.integer :user_id
       t.integer :tag_id, null: false
       t.integer :community_id, null: false
