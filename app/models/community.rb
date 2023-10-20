112140fb76c4560615ab991c78284fb2c112743c
Fuka <140349248+Oota1733@users.noreply.github.com>
2023-10-20 11:50:40 +0000

    add 修正２

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,5 @@
 class Community < ApplicationRecord
-
-  #has_many :entry_users, dependent: :destroy
+  
   belongs_to :user, class_name: 'User'
   has_many :users, through: :entry_users
   has_many :posts
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(email: "test@test.com", password: "aaaaaaaa")
+#  AdminのEメールとパスワード
+Admin.create!(email: "1234@1234.com", password: "12345678")
+
+#ユーザー作成
+maria = User.find_or_create_by!(email: "maria@example.com") do |user|
+  user.name = "maria"
+  user.password = "m1234567"
+end
+
+太郎 = User.find_or_create_by!(email: "taro@example.com") do |user|
+  user.name = "taro"
+  user.password = "i1234567"
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "y1234567"
+end
+
+
+#タグの作成
+tags = %w(現代文　数学　物理)
+tags.each { |tag_name| ActsAsTaggableOn::Tag.find_or_create_by(name: tag_name) }
+
+
+
+
