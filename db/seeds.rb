b81236610a5a62e225de2997351ff16d3117266a
Fuka <140349248+Oota1733@users.noreply.github.com>
2023-10-21 08:20:51 +0000

    add　seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,68 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #  AdminのEメールとパスワード
-Admin.create!(email: "1234@1234.com", password: "12345678")
+  Admin.find_or_create_by!(email: "1234@1234.com") do |a|
+    a.password = "12345678"
+  e
+  #ユーザーアカウント配列
 
-#ユーザー作成
-maria = User.find_or_create_by!(email: "maria@example.com") do |user|
-  user.name = "maria"
-  user.password = "m1234567"
-end
+  User.find_or_create_by!(email: "maria@example.com") do |u|
+    u.password = "m1234567"
+    u.last_name = "三木"
+    u.first_name = "まりあ"
+    u.last_name_kana = "みき"
+    u.first_name_kana = "まりあ"
+    u.phone_number = 12345678
+    u.postal_code = 1234567
+    u.address = "神奈川１"
+  end
 
-太郎 = User.find_or_create_by!(email: "taro@example.com") do |user|
-  user.name = "taro"
-  user.password = "i1234567"
-end
+  User.find_or_create_by!(email: "taro@example.com") do |u|
+    u.password = "i1234567"
+    u.last_name = "山田"
+    u.first_name = "太郎"
+    u.last_name_kana = "やまだ"
+    u.first_name_kana = "たろう"
+    u.phone_number = 87654321
+    u.postal_code = 1234567
+    u.address = "千葉１"
+  end
 
-lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
-  user.name = "Lucas"
-  user.password = "y1234567"
-end
+  User.find_or_create_by!(email: "kimura@example.com") do |u|
+    u.password = "y1234567"
+    u.last_name = "木村"
+    u.first_name = "りえ"
+    u.last_name_kana = "きむら"
+    u.first_name_kana = "りえ"
+    u.phone_number = 1234567
+    u.postal_code = 1234567
+    u.address = "東京１"
+  end
 
 
-#タグの作成
-tags = %w(現代文　数学　物理)
-tags.each { |tag_name| ActsAsTaggableOn::Tag.find_or_create_by(name: tag_name) }
+  # タグの作成
+  tags = %w(現代文 数学 物理 地理)
+  tags.each { |tag_name| Tag.find_or_create_by(name: tag_name) }
+
+  # User.all.each do |user|
+  #   Community.find_or_create_by!(user_id: user.id) do |p|
+  #   p.name = "ルーム１"
+  #   p.introduction = "こんにちは"
+  #   end
+  # end
 
 
+#    投稿を作成または既存のものを検索
+#     post = Post.find_or_create_by!(community_id: community.id) do |p|
+#       p.title = "こんんちは"
+#       p.body = "こんばんは"
+#       p.tag_list.add(tag_list) # タグを追加します。
+#       p.user_id = user.id
+#     end
+
+#     comment = Comment.find_or_create_by!(post_id: post.id) do |p|
+#       p.body = "今日"
+#       p.user_id = user.id
+#     end
 
 
